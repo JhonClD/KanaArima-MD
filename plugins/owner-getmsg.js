49277b638291358911e48ed10b4fb26c8481ab39
Jhon <165972049+JhonClD@users.noreply.github.com>
2026-04-22 18:13:01 -0500

    Refactor owner-getmsg handler for better error handling

--- plugins/owner-getmsg.js
+++ plugins/owner-getmsg.js
@@ -1,20 +1,31 @@
-
+import fs from 'fs'
 
 const handler = async (m, {conn, command, usedPrefix, text}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.owner_getmsg
+  try {
+    const datas = global
+    const idioma = datas.db.data.users[m.sender]?.language || global.defaultLenguaje || 'es'
+    const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
+    const tradutor = _translate.plugins.owner_getmsg
 
-  const which = command.replace(/ver/i, '');
-  if (!text) throw `${tradutor.texto1[0]} *${usedPrefix}list${which}* ${tradutor.texto1[1]}`;
-  const msgs = global.db.data.msgs;
-  if (!text in msgs) throw `*[❗𝐈𝐍𝐅𝐎❗] '${text}' ${tradutor.texto2}`;
-  const _m = await conn.serializeM(msgs[text]);
-  await _m.copyNForward(m.chat, true);
-};
-handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map((v) => 'get' + v + ' <text>');
-handler.tags = ['database'];
-handler.command = /^ver(vn|msg|video|audio|img|sticker)$/;
-handler.rowner = true;
-export default handler;
+    const which = command.replace(/ver/i, '')
+    if (!text) throw `${tradutor.texto1[0]} *${usedPrefix}list${which}* ${tradutor.texto1[1]}`
+    
+    const msgs = global.db.data.msgs
+    
+    if (!(text in msgs)) throw `*[❗𝐈𝐍𝐅𝐎❗] '${text}' ${tradutor.texto2}`
+    
+    const _m = await conn.serializeM(msgs[text])
+    await _m.copyNForward(m.chat, true)
+    
+  } catch (e) {
+    console.error('[GETMSG]', e)
+    throw e
+  }
+}
+
+handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map(v => `ver${v} <nombre>`)
+handler.tags = ['database']
+handler.command = /^ver(vn|msg|video|audio|img|sticker)$/i
+handler.rowner = true
+
+export default handler
